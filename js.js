af467c0a093031b28e99dd1c21b45dfc2c0cba92
KrylovArseny <46763160+KrylovArseny@users.noreply.github.com>
2020-10-09 22:03:58 +0300

    Update js.js

--- js.js
+++ js.js
@@ -7,16 +7,12 @@ function soap() {
       '<?xml version="1.0" encoding="utf-8"?>' +
       '<soapenv:Envelope ' + 
           'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-          'xmlns:api="http://127.0.0.1/Integrics/Enswitch/API" ' +
           'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
-          'xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' +
-          '<soapenv:Body>' +
-              '<api:some_api_call soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">' +
-                  '<username xsi:type="xsd:string">login_username</username>' +
-                  '<password xsi:type="xsd:string">password</password>' +
-              '</api:some_api_call>' +
-          '</soapenv:Body>' +
-      '</soapenv:Envelope>';
+          'xmlns:soap12="http://www.w3.org/2003/05/soap-envelope>' +
+          '<soap12:Body>' +
+              '<GetLatestDateTime xmlns="http://web.cbr.ru/" />'+
+          '</soap12:Body>' +
+      '</soap12:Envelope>';
 
   xmlhttp.onreadystatechange = function () {
       if (xmlhttp.readyState == 4) {
@@ -26,7 +22,7 @@ function soap() {
       }
   }
   // Send the POST request
-  xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+  xmlhttp.setRequestHeader('Content-Type', 'application/soap+xml; charset=utf-8');
   xmlhttp.send(sr);
   // send request
   // ...
